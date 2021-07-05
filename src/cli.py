d8c27c964ad47341b9f4e59cd08809cc0713c017
felipe <felipepassada@gmail.com>
2021-07-04 23:10:15 -0300

    Add argument for wich OS the script should install and configure packages

--- src/cli.py
+++ src/cli.py
@@ -1,6 +1,24 @@
 import click
+import subprocess
 
 @click.command()
-def cli():
-    """This Script helps you to prepare and configure your environment"""
-    click.echo('### Env setup tool ###')
+@click.argument("os")
+def cli(os):
+  """
+    This Script helps you to prepare and configure your environment
+  """
+  languages = 'nodejs, java'
+  click.echo('### Env setup tool ###')
+
+  if (os.casefold() == "Fedora".casefold()):
+    configure_install_fedora()
+
+def configure_install_fedora(): 
+  subprocess.run(['sudo', 'dnf', 'install', 'htop'])
+
+def configure_install_mac_os():
+  subprocess.run([])
+
+
+if __name__ == "__cli__":
+   cli()
