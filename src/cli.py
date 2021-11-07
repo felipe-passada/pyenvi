afe71f0ab0389d531d834972a6673893c38902cd
felipe <felipepassada@gmail.com>
2021-11-07 00:21:10 -0300

    [feat]: list languages and install them - add question for languages and tools to be installed - add a loop and fix the issue on how to pass a list of commands to   subprocess

--- src/cli.py
+++ src/cli.py
@@ -1,5 +1,57 @@
 import click
+from PyInquirer import prompt, Separator
 import subprocess
+"""
+TODO: 
+- show languages list to be installed (maybe use pyinquerer)
+- show container tools to be installed eg: 'docker, kubernets, etc...'
+"""
+questions = [
+  {
+    'type': 'checkbox',
+    'message': 'Select Languages',
+    'name': 'languages',
+    'choices': [
+    Separator('= Programming Languages ='),
+    {
+      'name': 'NodeJs',
+      'value': 'nodejs'
+    },
+    {
+      'name': 'Java',
+      'value': 'java'
+    },
+    {
+      'name': 'Python',
+      'value': 'python'
+    },
+  ],
+  'validate': lambda answer: 'You must choose at least one topping.' \
+    if len(answer) == 0 else True
+  },
+  {
+    'type': 'checkbox',
+    'message': 'Select container tools',
+    'name': 'container',
+    'choices': [
+    Separator('= Container tooling ='),
+    {
+      'name': 'Docker',
+      'value': 'docker'
+    },
+    {
+      'name': 'Podman',
+      'value': 'podman'
+    },
+    {
+      'name': 'K8\'s',
+      'value': 'kubernets'
+    },
+  ],
+  'validate': lambda answer: 'You must choose at least one topping.' \
+    if len(answer) == 0 else True
+  }
+]
 
 @click.command()
 @click.argument("os")
@@ -7,14 +59,23 @@ def cli(os):
   """
     This Script helps you to prepare and configure your environment
   """
-  languages = 'nodejs, java'
   click.echo('### Env setup tool ###')
 
-  if (os.casefold() == "Fedora".casefold()):
-    configure_install_fedora()
+  # if (os.casefold() == "Fedora".casefold()):
+    # configure_install_fedora(languages)
+  answner = prompt(questions)
+  languages, tools = answner.values()
+  print("Languages to be installed: ", languages, " tools: ", tools)
+  for language in languages:
+    subprocess.run(['sudo', 'dnf', 'install', language])
 
-def configure_install_fedora(): 
-  subprocess.run(['sudo', 'dnf', 'install', 'htop'])
+
+#TODO:
+# - Change this function to install_linux
+# - Create a distro dictionary to match the distro an then replace the pkg-mng
+# eg: `subprocess.run(['sudo', pkg-mng, 'install', languages)`
+def configure_install_fedora(languages): 
+  subprocess.run(['sudo', 'dnf', 'install', languages[0]])
 
 def configure_install_mac_os():
   subprocess.run([])
